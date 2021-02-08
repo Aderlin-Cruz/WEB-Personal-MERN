a072ecb3a003fe8fe277f047c570178a033be4b8
Aderlin-Cruz <aderlin.cruz95@gmail.com>
2021-02-08 16:41:15 -0600

    version 1

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const mongoose= require("mongoose");
+const app= require("./app");
+const port= process.env.PORT || 3977;
+const {API_VERSION, IP_SERVER,PORT_DB}= require("./config");
+
+//mongoose.connect('mongodb://localhost:27017/cory');
+
+
+mongoose.connect(
+'mongodb://${IP_SERVER}:${PORT_DB}/cory',
+{useNewUrlParser:true},
+(err,res) => {
+  if(err){
+    throw err;
+
+  }
+  else{
+
+    console.log("La conexión a la base de datos es correcta.");
+    app.listen(port, ()=>{
+      console.log("##########################");
+      console.log("######## API REST ########");
+      console.log("##########################");
+      console.log("htpp://{IP_SERVER}:${port}/api/${API_VERSION}/");
+
+
+     });
+    }
+
+
+  }
+);
+
+
+
+
+
---
+++ web-cliente/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import logo from './logo.svg';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Web </h1>
+     <h2>Proyecto</h2>
+     
+    </div>
+  );
+}
+
+export default App;
